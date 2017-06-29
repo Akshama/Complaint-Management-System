3c2e5c20fdeeca48a7fcd614954e49a3fd469fe0
kary7124 <puri_hema12@yahoo.com>
2017-06-29 21:09:19 +0530

    Admin Login

---
+++ CmsLogin/app/src/main/java/com/tcs/cmslogin/Logout.java
@@ -0,0 +1,25 @@
+package com.tcs.cmslogin;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by hema on 28-Jun-17.
+ */
+
+public class Logout extends Fragment {
+
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        myView=inflater.inflate(R.layout.logout,container,false);
+        return myView;
+    }
+}
