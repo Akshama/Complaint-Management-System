5ed61d2590a6d956f4ba80a857fe7c7b02c1f2b4
Akshama <akshama96@gmail.com>
2017-07-15 14:53:02 +0530

    Engineer Segment

---
+++ Engineer-Navigation/app/src/main/java/com/example/akshamapc/engineer_navigation/ContactAdmin.java
@@ -0,0 +1,44 @@
+package com.example.akshamapc.engineer_navigation;
+
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+/**
+ * Created by Akshama PC on 7/12/2017.
+ */
+public class ContactAdmin extends android.support.v4.app.Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //returning our layout file
+        //change R.layout.yourlayoutfilename for each of your fragments
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.setType("message/rfc822");
+        i.putExtra(Intent.EXTRA_EMAIL  , new String[]{"admin@tcs.com"});
+        i.putExtra(Intent.EXTRA_SUBJECT, "subject of email");
+        i.putExtra(Intent.EXTRA_TEXT, "body of email");
+        try {
+            startActivity(Intent.createChooser(i, "Send mail..."));
+        } catch (android.content.ActivityNotFoundException ex) {
+
+        }
+        return inflater.inflate(R.layout.fragment_contact_admin, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //you can set the title for your toolbar here for different fragments different titles
+        getActivity().setTitle("Contact Admin");
+
+    }
+}
+
